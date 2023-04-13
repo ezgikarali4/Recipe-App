677e8029145f0c363524b4eec6bb57ca6f2d0a85
ezgikarali <ezgi.karali505@gmail.com>
2023-04-13 13:32:26 +0300

    new api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "59138afe-74d4-477e-83f0-207a62556ca4";
+export const KEY = "2b8982dd-ab3a-4c9e-95d2-16db571544f9";
 export const MODAL_CLOSE_SEC = 2.5;
 
 //https://forkify-api.herokuapp.com/v2
